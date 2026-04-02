d2166d34f3f49b852c525f35c3c7d86e05245b31
Genc Vllahiu <genc.v@gjirafa.com>
2026-04-02 12:36:49 +0200

    adding thumbnail and 3-Clause BSD License

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
 import deckyPlugin from "@decky/rollup";
 
-export default deckyPlugin();
+const config = deckyPlugin();
+
+delete config.output.dir;
+config.output.file = "dist/index.js";
+config.output.format = "iife";
+export default config;
